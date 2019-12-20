c994cac49c983d7638d71644720db431f56ebef4
sutinan liamthong <zero19223@gmail.com>
2019-12-20 13:12:44 +0700

    Python v.1
    1

---
+++ MyApp.py
@@ -0,0 +1,36 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text='username'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+        self.submit = Button(text ='Login')
+        self.add_widget(self.submit)
+        self.submit.bind(on_press=self.pressed)
+
+    def pressed(self, instance):
+        username = self.username.text
+        print(username)
+        password = self.password.text
+        print(password)
+
+class MyApp(App):
+
+    def build(self):
+        return LoginScreen()
+
+
+if __name__ == '__main__':
+    MyApp().run()
+
